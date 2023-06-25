087a7645a43e87bae17acbed082ae518ac9574e6
Keksi <contact@keksi.me>
2023-06-25 11:56:05 +0200

    chore: add favicon and og

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vitepress'
 
+const ogUrl = 'https://lona.js.org/'
+const ogImage = `${ogUrl}og.png#1`
+const title = 'LonaDB'
+const description = 'JSON based Database written in JavaScript'
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'alternate icon', href: '/favicon.ico', type: 'image/png', sizes: '16x16' }],
+    ['meta', { name: 'author', content: 'Keksi' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { name: 'og:title', content: title }],
+    ['meta', { name: 'og:description', content: description }],
+    ['meta', { property: 'og:image', content: ogImage }],
+    ['meta', { name: 'twitter:title', content: title }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:image', content: ogImage }],
+    ['meta', { name: 'twitter:site', content: '@keksiqc' }],
+    ['meta', { name: 'twitter:url', content: ogUrl }],
+  ],
   title: "LonaDB",
   description: "JSON based Database written in JavaScript",
   themeConfig: {
