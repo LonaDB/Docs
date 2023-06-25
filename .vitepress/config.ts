db4e2fa40077e75b1cdbe89364583b43375e46dd
Collin Buchkamer <collinbuchkamer@gmail.com>
2023-06-25 11:36:35 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   title: "LonaDB",
   description: "JSON based Database written in JavaScript",
   themeConfig: {
+    logo: 'https://raw.githubusercontent.com/LonaDB/Website/main/.vitepress/logo_lonadb.png',
+    
     search: {
       provider: 'local'
     },
